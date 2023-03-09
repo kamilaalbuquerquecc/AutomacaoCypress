fd7b236531defdc399e43e9d2ff7cd8f2781416e
Kamila <kamila.albuquerque@ccc.ufcg.edu.br>
2023-03-09 15:30:18 -0300

    Aprimorando os testes

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -2,37 +2,62 @@
 
 describe('tarefas', () => {
 
-    it('deve cadastrar uma nova tarefa', () => {
-        const taskName = 'Ler um livro node.js'
+    context('cadastro', () => {
+        it('deve cadastrar uma nova tarefa', () => {
+            const taskName = 'Ler um livro node.js'
 
-        cy.removeTaskByName(taskName)
+            cy.removeTaskByName(taskName)
 
-        cy.createTask(taskName)
+            cy.createTask(taskName)
 
-        cy.contains('main div p', taskName)
-            .should('be.visible')
+            cy.contains('main div p', taskName)
+                .should('be.visible')
+        })
+
+        it('não deve permitir tarefa duplicada', () => {
+            const task = {
+                name: 'Estudar Javascript',
+                is_done: false
+            }
+
+            cy.removeTaskByName(task.name)
+
+            cy.postTask(task)
+
+            cy.createTask(task.name)
+
+            cy.get('.swal2-html-container')
+                .should('be.visible')
+                .should('have.text', 'Task already exists!')
+        })
+
+        it('campo obrigatório', () => {
+            cy.createTask()
+            cy.isRequired('This is a required field')
+        })
     })
+    context('atualizaçao', () => {
+        it('deve concluir uma tarefa', () => {
+            const task = {
+                name: "Pagar contas", is_done: false}
 
-    it('não deve permitir tarefa duplicada', () => {
-        const task = {
-            name: 'Estudar Javascript',
-            is_done: false
-        }
+            cy.removeTaskByName(task.name)
+            cy.postTask(task)
 
-        cy.removeTaskByName(task.name)
+            cy.visit('http://localhost:8080')
 
-        cy.postTask(task)
+            cy.contains('p', task.name)
+                .parent()
+                .find('button[class*=ItemToggle]')
+                .click()
+                //should('be.visible')
+            
+            cy.contains('p', task.name)
+                .should('have.css', 'text-decoration-line', 'line-through')
+            
+            
 
-        cy.createTask(task.name)
+        })
 
-        cy.get('.swal2-html-container')
-            .should('be.visible')
-            .should('have.text', 'Task already exists!')
     })
-
-    it('campo obrigatório', () => {
-        cy.createTask()
-        cy.isRequired('This is a required field')
-    })
-
 })
